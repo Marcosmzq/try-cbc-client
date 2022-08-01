dc27b79228f0dd84fe1f3d70eec1279750d2c207
Marcos Martinez Quiroga <marcosmartinez8@outlook.com>
2022-08-01 13:32:20 -0300

    Add - Now the close button of the header is red

--- src/components/molecules/active-header-menu/default/index.tsx
+++ src/components/molecules/active-header-menu/default/index.tsx
@@ -8,81 +8,82 @@ interface ActiveHeaderMenuDefaultProps {
   closeMenu: () => void;
 }
 
-export const ActiveHeaderMenuDefault: React.FC<ActiveHeaderMenuDefaultProps> =
-  ({ closeMenu }) => {
-    const { user, logout } = useContext(AuthContext);
-    return (
-      <Flex
-        pos="fixed"
-        top="0"
-        bg="white"
-        minH="100vh"
-        w="100%"
-        zIndex="overlay"
-        p={24}
-        flexDir="column"
-      >
-        <Flex justify="space-between" w="100%">
-          <CloseButton size="lg" onClick={closeMenu} />
-        </Flex>
-        <VStack my={12} spacing={12}>
-          <HeaderNavLinks
-            navLinkItems={
-              user
-                ? [
-                    { text: "Inicio", href: "/home", onClick: closeMenu },
-                    {
-                      text: "Perfil",
-                      href: `/profile`,
-                      onClick: closeMenu,
-                    },
-                    {
-                      text: "Premiun",
-                      href: `/premium`,
-                      onClick: closeMenu,
-                    },
-                  ]
-                : [
-                    { text: "Inicio", href: "/", onClick: closeMenu },
-                    {
-                      text: "Premiun",
-                      href: `/premium`,
-                      onClick: closeMenu,
-                    },
-                  ]
-            }
-          />
-          <HeaderCTA
-            headerCtaItems={
-              user
-                ? [
-                    {
-                      href: "/",
-                      text: "Cerrar sesion",
-                      btnVariant: "primarySolid",
-                      onClick: () => {
-                        logout();
-                        closeMenu();
-                      },
-                    },
-                  ]
-                : [
-                    {
-                      href: "/login",
-                      text: "Iniciar sesion",
-                      btnVariant: "primaryOutline",
-                      onClick: closeMenu,
-                    },
-                    {
-                      href: "/register",
-                      text: "Crear cuenta",
-                      btnVariant: "primarySolid",
-                      onClick: closeMenu,
-                    },
-                  ]
-            }
-          />
-        </VStack>
+export const ActiveHeaderMenuDefault: React.FC<
+  ActiveHeaderMenuDefaultProps
+> = ({ closeMenu }) => {
+  const { user, logout } = useContext(AuthContext);
+  return (
+    <Flex
+      pos="fixed"
+      top="0"
+      bg="white"
+      minH="100vh"
+      w="100%"
+      zIndex="overlay"
+      p={16}
+      flexDir="column"
+    >
+      <Flex justify="flex-end" w="100%">
+        <CloseButton color="red" size="lg" onClick={closeMenu} />
       </Flex>
-    );
-  };
+      <VStack my={12} spacing={12}>
+        <HeaderNavLinks
+          navLinkItems={
+            user
+              ? [
+                  { text: "Inicio", href: "/home", onClick: closeMenu },
+                  {
+                    text: "Perfil",
+                    href: `/profile`,
+                    onClick: closeMenu,
+                  },
+                  {
+                    text: "Premiun",
+                    href: `/premium`,
+                    onClick: closeMenu,
+                  },
+                ]
+              : [
+                  { text: "Inicio", href: "/", onClick: closeMenu },
+                  {
+                    text: "Premiun",
+                    href: `/premium`,
+                    onClick: closeMenu,
+                  },
+                ]
+          }
+        />
+        <HeaderCTA
+          headerCtaItems={
+            user
+              ? [
+                  {
+                    href: "/",
+                    text: "Cerrar sesion",
+                    btnVariant: "primarySolid",
+                    onClick: () => {
+                      logout();
+                      closeMenu();
+                    },
+                  },
+                ]
+              : [
+                  {
+                    href: "/login",
+                    text: "Iniciar sesion",
+                    btnVariant: "primaryOutline",
+                    onClick: closeMenu,
+                  },
+                  {
+                    href: "/register",
+                    text: "Crear cuenta",
+                    btnVariant: "primarySolid",
+                    onClick: closeMenu,
+                  },
+                ]
+          }
+        />
+      </VStack>
+    </Flex>
+  );
+};
